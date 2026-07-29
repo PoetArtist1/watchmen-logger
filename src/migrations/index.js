93b15349d9a08dbe5244bb2bf5a1fdb9edfd87b5
Carlos Campos <carloshightower0502@gmail.com>
2026-07-29 13:23:32 -0400

    feat(migrations): add SQL schemas and migration runner for requests and manual_logs tables

---
+++ src/migrations/index.js
@@ -0,0 +1,8 @@
+/**
+ * @module migrations
+ * @description Public API for the migrations subsystem.
+ */
+
+const MigrationRunner = require('./MigrationRunner');
+
+module.exports = { MigrationRunner };
