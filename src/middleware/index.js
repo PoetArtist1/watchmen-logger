8ac983b8ee40fe9881a93d090de79c5535902215
Carlos Campos <carloshightower0502@gmail.com>
2026-07-29 13:24:23 -0400

    feat(middleware): implement automatic request/response capture middleware for Express

---
+++ src/middleware/index.js
@@ -0,0 +1,8 @@
+/**
+ * @module middleware
+ * @description Public API for the middleware subsystem.
+ */
+
+const createCaptureMiddleware = require('./captureMiddleware');
+
+module.exports = { createCaptureMiddleware };
